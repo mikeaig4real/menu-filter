c3a370018efeaaf3e1f968bbd871e9507fd4c219
Gbengashowunmi <gbengashowunmi01@gmail.com>
2022-04-17 18:03:42 +0100

    new menu

---
+++ script.js
@@ -0,0 +1,171 @@
+const frame = [
+    {
+        category : 'breakfast',
+        image: "/images/image_2.jpg",
+        title : 'Bread and Beans',
+        price : '50',
+        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quia architecto ipsa quis provident iste quibusdam consequuntur in quae Lorem ipsum dolor sit, amet consectetur adipisicing elit..' 
+    },
+    {
+        category : 'breakfast',
+        image: "/images/image_1.jpg",
+        title : 'Bread and Beans',
+        price : '53',
+        details: ' Commodi labore repellat excepturi earum sequi molestias repudiandae non odio ea quisquam ab voluptates dicta debitis quo, tempora totam nam magni similique consequatur est at placeat facilis' 
+    },
+    {
+        category : 'lunch',
+        image: "/images/image_3.jpg",
+        title : 'Bread and Beans',
+        price : '65',
+        details: 'Autem assumenda eaque sit voluptatem consectetur dolorum et. Ducimus quam quos vero ipsum illum dicta in veniam quod voluptate recusandae aut assumenda voluptatibus quo, libero, delectus facere illo rerum labore! Tempora voluptatum, distinctio accusamus corrupti autem perspiciatis nisi fugiat maiores' 
+    },
+    {
+        category : 'shakes',
+        image: "/images/image_4.jpeg",
+        title : 'Bread and Beans',
+        price : '48',
+        details: 'quo repudiandae. Inventore, neque tenetur sit nesciunt consequuntur, optio illum fuga modi eligendi non corporis dolor explicabo animi. Dolorum unde alias molestiae quod, ad labore aspernatur nam harum, voluptas minus explicabo? Reiciendis corrupti quia quam voluptatibus dolore possimus eligendi' 
+    },
+    {
+        category : 'dinner',
+        image: "/images/image_5.jpeg",
+        title : 'Bread and Beans',
+        price : '83',
+        details: 'aperiam vel. Nam, dolorem culpa? Sequi est libero nemo blanditiis dolores aliquid optio! Ut delectus officiis, laboriosam et accusantium ipsam neque facere est tempore adipisci sapiente, nesciunt molestiae vel libero! Cupiditate vitae ut dolores eaque repudiandae debitis, eius culpa temporibus dolorem modi nam ipsam necessitatibus!' 
+    },
+    {
+        category : 'shakes',
+        image: "/images/image_6.jpeg",
+        title : 'Bread and Beans',
+        price : '36',
+        details: 'praesentium vel minima dolores alias. Ab repellendus neque a illum, eos nostrum doloribus et tenetur. Fugit obcaecati suscipit voluptas animi possimus illum dolore expedita explicabo temporibus non autem magnam iure maiores quis libero vitae, dicta nihil officia quisquam perspiciatis molestiae,' 
+    },
+    {
+        category : 'lunch',
+        image: "/images/image_1.jpg",
+        title : 'Bread and Beans',
+        price : '29',
+        details: 'rem nihil repellendus, doloremque molestias dolores doloribus vel fuga voluptatibus quisquam dolore cupiditate. Fuga consequatur magni quod accusamus eaque, vel natus atque nisi. Harum qui, corrupti eaque doloribus quis tempore adipisci voluptates dolorum asperiores mollitia est, nulla debitis officia' 
+    },
+    {
+        category : 'shakes',
+        image: "/images/image_2.jpg",
+        title : 'Bread and Beans',
+        price : '14',
+        details: 'voluptate obcaecati sint recusandae quisquam totam enim! Ea, asperiores! Blanditiis incidunt fugiat doloribus atque mollitia voluptatibus voluptas suscipit amet, earum natus commodi ex doloremque architecto temporibus alias! Earum omnis reiciendis adipisci, in molestias cumque' 
+    },
+    {
+        category : 'dinner',
+        image: "/images/image_3.jpg",
+        title : 'Bread and Beans',
+        price : '67',
+        details: 'doloremque nam. Iusto ducimus voluptatibus, officia reiciendis possimus unde doloremque error, quod magnam labore ut exercitationem vitae rerum vel nihil optio voluptas mollitia! Atque eum ducimus possimus, dicta fugiat cupiditate? Aliquid maiores dolorum atque rerum excepturi amet ad, harum,' 
+    },
+    {
+        category : 'lunch',
+        image: "/images/image_4.jpeg",
+        title : 'Bread and Beans',
+        price : '37',
+        details: 'animi id accusantium temporibus nam esse nemo nobis aliquam ex molestiae eum velit distinctio facere maxime perspiciatis! Beatae vero a voluptatum assumenda accusantium ducimus aliquid nam omnis maiores adipisci quaerat et, repudiandae dolorum recusandae voluptatibus eaque veniam saepe, quia commodi' 
+    },
+    {
+        category : 'dinner',
+        image: "/images/image_5.jpeg",
+        title : 'Bread and Beans',
+        price : '61',
+        details: 'Beatae, repellat hic. Ipsa quisquam nostrum aperiam, veritatis assumenda debitis voluptate commodi incidunt eligendi consectetur nesciunt molestias adipisci unde repellendus at velit sint ducimus odit, quo tenetur aliquid laborum minus tempora! Quae at iste odit, recusandae id cupiditate' 
+    },
+    {
+        category : 'lunch',
+        image: "/images/image_6.jpeg",
+        title : 'Bread and Beans',
+        price : '43',
+        details: ' Distinctio, qui ipsam. Dolore, ad ducimus aut eligendi doloribus ipsum consequuntur quam harum consectetur odit vitae eius dignissimos, vero, aliquid est.  obcaecati ad quibusdam ratione cum quod, error delectus culpa iusto fuga perspiciatis voluptatem beatae pariatur reprehenderit corporis dicta' 
+    },
+    {
+        category : 'breakfast',
+        image: "/images/image_1.jpg",
+        title : 'Bread and Beans',
+        price : '73',
+        details: 'Repellat illo optio doloribus, sapiente animi quos possimus aspernatur reprehenderit autem magnam dolore, nostrum commodi odit nulla beatae quaerat id molestiae, omnis quam accusantium quo? Officia iusto repellendus aliquid et a, repudiandae saepe aut unde officiis at laboriosam, veniam' 
+    },
+    {
+        category : 'dinner',
+        image: "/images/image_2.jpg",
+        title : 'Bread and Beans',
+        price : '32',
+        details: 'Explicabo odio quaerat labore deleniti consequuntur facilis commodi nemo minima at, deserunt iure soluta harum repellat adipisci, vitae voluptatibus eum veritatis autem vel nostrum hic quia dolore eligendi perferendis. quasi?  dolorem asperiores nihil maiores. Ab, eius' 
+    },
+    {
+        category : 'breakfast',
+        image: "/images/image_5.jpeg",
+        title : 'Bread and Beans',
+        price : '63',
+        details: 'Delectus repellat adipisci eum autem incidunt quam distinctio consectetur laboriosam neque minus, unde explicabo nihil!  id dignissimos sit voluptates praesentium, magni asperiores error iure facilis rerum, reiciendis iusto! Culpa dolore eos ex omnis voluptatem reprehenderit.' 
+    }
+];
+
+const container = document.querySelector('.main-container');
+const buttons = document.querySelectorAll('.btns');
+
+
+//load items
+// window.addEventListener('DOMContentLoaded',function()
+// {
+//    displayFrameItem(frame);
+// });
+
+//filter items
+buttons.forEach(function(btn){
+    btn.addEventListener('click', function(e) {
+        const cat = e.target.innerText;
+        // console.log(cat);
+    //    const frameCat = frame.filter(({category})=> category == cat);
+       console.log(frame.filter(({category})=> category == cat),  frame); 
+           
+       })
+       
+
+
+
+    //        if (frames.category === category) {
+    //         return frames;
+    //        }
+    //    }); 
+    //    if (cat === category) {
+    //     console.log(frame);
+    //     displayFrameItem(frame);
+    //    }
+
+    //    else{
+    //     console.log(frameCat);
+    //     displayFrameItem(frameCat);
+    //    }
+    //  });
+        
+        
+});
+// });
+
+function displayFrameItem(frameItem) {
+    let displayFrame = frame.map(function(item){
+    
+        return   `<div class="container">
+            <img src=${item.image} alt=${item.title}>
+        <div class="info">
+            <div class="title_price">
+                <p >${item.title}</p>
+                <p class="price">$${item.price}</p>
+            </div>
+            <p class="details">${item.details}</p>
+            </div>
+    
+        </div>`;
+       });
+    
+    
+       displayFrame = displayFrame.join('')
+    
+       container.innerHTML = displayFrame;
+};
